8f4a1bc57ae7002cba7fa964acfb16a7fce6b134
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-05 17:18:34 -0400

    can now keep track of an inning through outs and team scores

--- baseball-counter-testing/src/App.js
+++ baseball-counter-testing/src/App.js
@@ -10,10 +10,32 @@ import './App.css';
 function App() {
   const [ballCounter, setBallCounter] = useCounter('ball');
   const [strikeCounter, setStrikeCounter] = useCounter('strike');
+  const [outCounter, setOutCounter] = useCounter('outs');
+  const [team, setTeam] = useCounter('team');
+  const [scoreZero, setScoreZero] = useCounter('scoreZero');
+  const [scoreOne, setScoreOne] = useCounter('scoreOne');
 
   const resetPoints = () => {
     setBallCounter(0);
     setStrikeCounter(0);
+    if (outCounter < 2) {
+      setOutCounter(incrementByOne(outCounter));
+    }
+    else {
+      setOutCounter(0);
+      team === 0 ? setTeam(1) : setTeam(0);
+    }
+  }
+
+  const score = () => {
+    setBallCounter(0);
+    setStrikeCounter(0);
+    if (team == 0) {
+      setScoreZero(incrementByOne(scoreZero));
+    }
+    else {
+      setScoreOne(incrementByOne(scoreOne));
+    }
   }
 
   const incrementPoints = (type) => {
@@ -43,7 +65,7 @@ function App() {
     }
     //hit logic
     else if (type === 'hit') {
-      resetPoints();
+      score();
     }
     else {
       alert('Error: Input Unrecognized: App.js: incrementPoints Function');
@@ -52,7 +74,14 @@ function App() {
 
   return (
     <div className="App" >
-      <Display ballCounter={ballCounter} strikeCounter={strikeCounter} />
+      <Display
+        ballCounter={ballCounter}
+        strikeCounter={strikeCounter}
+        outCounter={outCounter}
+        team={team}
+        scoreZero={scoreZero}
+        scoreOne={scoreOne}
+      />
       <Dashboard incrementPoints={incrementPoints} />
     </div >
   );
--- baseball-counter-testing/src/components/Display.js
+++ baseball-counter-testing/src/components/Display.js
@@ -5,8 +5,13 @@ const Display = (props) => {
     return (
         <>
             <h1>Baseball Dashboard</h1>
+            <h2>Team: {props.team + 1}</h2>
+            <h3>Team 1 Score: {props.scoreZero}</h3>
+            <h3>Team 2 Score: {props.scoreOne}</h3>
             <h3>Ball: {props.ballCounter}</h3>
             <h3>Strike: {props.strikeCounter}</h3>
+            <h3>Outs: {props.outCounter}</h3>
+            <h3></h3>
         </>
     )
 }
