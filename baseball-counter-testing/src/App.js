82469f879eb6a7689cc10d759b00c0a33b2fc58f
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-05 15:37:15 -0400

    created customHook and set up ball and strike counter display

--- baseball-counter-testing/src/App.js
+++ baseball-counter-testing/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const handleValueChange = (newValue) => {
+    localStorage.setItem(key, newValue);
+    setStoredValue(newValue);
+  }
+
+  return [storedValue, handleValueChange];
+}
+
+const useCounter = (counterType) => {
+  const [counter, setCounter] = useLocalStorage(counterType, 0);
+
+  return [counter, setCounter];
+}
+
 function App() {
+  const [ballCounter, setBallCounter] = useCounter('ball');
+  const [strikeCounter, setStrikeCounter] = useCounter('strike');
+
   return (
     <div className="App">
-      Test
+      <h1>Baseball Dashboard</h1>
+      <h3>Ball: {ballCounter}</h3>
+      <h3>Strike: {strikeCounter}</h3>
     </div>
   );
 }
