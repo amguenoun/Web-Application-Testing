b73d4f37912443700946c3229a6d1cbf2a8a9a56
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-05 15:52:05 -0400

    set up app functionality and logic, error with foul button

--- baseball-counter-testing/src/App.js
+++ baseball-counter-testing/src/App.js
@@ -25,12 +25,58 @@ function App() {
   const [ballCounter, setBallCounter] = useCounter('ball');
   const [strikeCounter, setStrikeCounter] = useCounter('strike');
 
+  const resetPoints = () => {
+    setBallCounter(0);
+    setStrikeCounter(0);
+  }
+
+  const incrementPoints = (type) => {
+    //ball logic
+    if (type === 'ball') {
+      if (ballCounter === 3) {
+        resetPoints();
+      }
+      else {
+        setBallCounter(ballCounter + 1);
+      }
+    }
+    //strike logic
+    else if (type === 'strike') {
+      if (strikeCounter === 2) {
+        resetPoints();
+      }
+      else {
+        setStrikeCounter(strikeCounter + 1);
+      }
+    }
+    //foul logic
+    else if (type === 'foul') {
+      if (strikeCounter > 2) {
+        setStrikeCounter(strikeCounter + 1);
+        console.log("running")
+      }
+    }
+    //hit logic
+    else if (type === 'hit') {
+      resetPoints();
+    }
+    else {
+      alert('Error: Input Unrecognized: App.js: incrementPoints Function');
+    }
+  }
+
   return (
-    <div className="App">
+    < div className="App" >
       <h1>Baseball Dashboard</h1>
       <h3>Ball: {ballCounter}</h3>
       <h3>Strike: {strikeCounter}</h3>
-    </div>
+      <div className="buttonBar">
+        <button onClick={() => incrementPoints('strike')}>Strike</button>
+        <button onClick={() => incrementPoints('ball')}>Ball</button>
+        <button onClick={() => incrementPoints('foul')}>Foul</button>
+        <button onClick={() => incrementPoints('hit')}>Hit</button>
+      </div>
+    </div >
   );
 }
 
