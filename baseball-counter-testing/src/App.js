3981c39424798387fd33b72db82d3565bfe4b397
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-05 16:00:19 -0400

    moved custom hooks into seperate directory and files

--- baseball-counter-testing/src/App.js
+++ baseball-counter-testing/src/App.js
@@ -1,25 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import useCounter from './components/hooks/useCounter';
+
 import './App.css';
-
-const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
-  });
-
-  const handleValueChange = (newValue) => {
-    localStorage.setItem(key, newValue);
-    setStoredValue(newValue);
-  }
-
-  return [storedValue, handleValueChange];
-}
-
-const useCounter = (counterType) => {
-  const [counter, setCounter] = useLocalStorage(counterType, 0);
-
-  return [counter, setCounter];
-}
 
 function App() {
   const [ballCounter, setBallCounter] = useCounter('ball');
